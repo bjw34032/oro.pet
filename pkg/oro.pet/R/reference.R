635356757cb70a2f1687f814047971c0380aa3dd
bwhitcher <bwhitcher@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-04 17:07:58 +0000

    Added code and help files for MRTM and SRTM Added packages to suggests

--- pkg/oro.pet/R/reference.R
+++ pkg/oro.pet/R/reference.R
@@ -32,25 +32,53 @@
 ## $Id: $
 ##
 
-simplifiedReferenceTissueModel <- function(conc, conc.ref, time, mask,
-                                           k2ref=NULL) {
+simplifiedReferenceTissueModel <- function(tac, ref, time, SRTM2=TRUE,
+                                           k2prime=NULL,
+                                           guess=c("R1"=0.5, "k2"=0.01),
+                                           control=nls.lm.control()) {
   require("minpack.lm")
-  if (is.null(k2ref)) {
-    func.model <- compartmentalModel("srtm")
-    guess <- c("R1"=, "k2prime"=, "k2"=)
-  } else {
-    func.model <- compartmentalModel("srtm2")
-    guess <- c("R1"=, "k2"=)
-  }
-  func <- function(theta, signal, time, ...) {
-    out <- signal - func.model(time, theta, ...)
+  require("msm")
+  func <- function(theta, signal, time, ref, k2prime) {
+    out <- signal - extended.empirical(time, theta, ref, k2prime)
     out[!is.na(out)]
   }
-  nvoxels <- sum(mask)
-  
-  return(1)
+  out <- nls.lm(par=guess, fn=func, control=control, signal=tac, time=time,
+                ref=ref, k2prime=k2prime)
+  srtm2 <- list(BP = out$par[1] * k2prime / out$par[2] - 1,
+                R1 = out$par[1],
+                k2 = out$par[2])
+  srtm2 <- lapply(srtm2, as.numeric)
+  rdf <- length(out$fvec) - length(coef(out))
+  varcovmat <- (out$deviance / rdf) * chol2inv(chol(out$hessian))
+  c(srtm2, BP.error = deltamethod(~ x1 * k2prime / x2, out$par, varcovmat),
+    R1.error = sqrt(varcovmat[1,1]), k2.error = sqrt(varcovmat[2,2]))
 }
 
-multilinearReferenceTissueModel <- function(nim, mask, k2ref=NULL) {
-  return(1)
+multilinearReferenceTissueModel <- function(tac, ref, time, tstar,
+                                            MRTM2=TRUE, k2prime=NULL) {
+  require("msm")
+  ## Numeric integration
+  time.in.sec <- seq(min(0, time * 60), ceiling(max(time * 60)), by=1)
+  sec <- list(tac = approx(c(0, time) * 60, c(0, tac), time.in.sec)$y,
+              ref = approx(c(0, time) * 60, c(0, ref), time.in.sec)$y)
+  ## Fit linear model and estimate parameters
+  X <- cbind(cumsum(sec$ref) + sec$ref / (k2prime / 60), # in seconds
+             cumsum(sec$tac))[time * 60,]
+  dimnames(X) <- list(NULL, paste("gamma", 1:2, sep=""))
+  index <- time > tstar
+  fit <- lsfit(X[index,], tac[index], intercept=FALSE)
+  gamma <- fit$coefficients
+  mrtm2 <- list(BP = - (gamma[1] / gamma[2] + 1),
+                R1 = gamma[1] / k2prime,
+                k2 = - gamma[2])
+  mrtm2 <- lapply(mrtm2, as.numeric)
+  ## Construct variance-covariance matrix for regression parameters
+  rdf <- length(residuals(fit)) - length(coefficients(fit))
+  varcovbeta <- (sum(residuals(fit)^2) / rdf) * chol2inv(chol(t(X) %*% X))
+  ## Construct list output with approximate standard errors
+  list(BP = as.numeric(mrtm2$BP),
+       BP.error = deltamethod(~ x1 / x2, gamma, varcovbeta),
+       R1 = as.numeric(mrtm2$R1), R1.error = as.numeric(sqrt(varcovbeta[1,1])),
+       k2 = as.numeric(mrtm2$k2), k2.error = as.numeric(sqrt(varcovbeta[2,2])),
+       X = X, beta = gamma)
 }
